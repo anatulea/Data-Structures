06eeb13dde09c204fc305a91cfa4093c6a96b869
Ana Tulea <anatulea14@gmail.com>
2020-09-17 21:46:11 -0700

    added lecture notes

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -3,10 +3,16 @@ Each ListNode holds a reference to its previous node
 as well as its next node in the List.
 """
 class ListNode:
-    def __init__(self, value, prev=None, next=None):
-        self.prev = prev
+    def __init__(self, value, prev_node=None, next_node=None):
+        self.prev = prev_node
         self.value = value
-        self.next = next
+        self.next = next_node
+
+    def delete(self ):
+        if self.prev:
+            self.next.prev = self.prev
+        if self.next:
+            self.prev.next = self.next
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +33,21 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        # create a new node 
+        new_node = ListNode(value)
+        # 1. add to empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # 2. add to nonempty
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        # update length
+        self.length += 1
+           
+        
         
     """
     Removes the List's current head node, making the
@@ -58,7 +78,11 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        self.delete(node)
+        self.add_to_head(node.value)
+        
         
     """
     Removes the input node from its current spot in the 
@@ -72,8 +96,22 @@ class DoublyLinkedList:
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
-
+        # don't need to return value
+        # do need to update head, tail
+        if self.head is None:
+            return None 
+        elif self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node == self.head: # list has +2 nodes
+            self.head == node.next
+            node.delete() # uptades the prev and/or next 
+        elif node == self.tail:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+        self.length -=1
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
